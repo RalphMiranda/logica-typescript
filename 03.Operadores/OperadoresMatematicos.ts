5ba9a1c53e6455ab05af50958459d0522864d1d4
Ralph Oliveira de Miranda <ralph.miranda@gmail.com>
2023-12-16 22:03:50 -0300

    Operadores

---
+++ 03.Operadores/OperadoresMatematicos.ts
@@ -0,0 +1,14 @@
+let primeiroNumero :number = 10;
+let segundoNumero :number = 4;
+
+let soma :number = primeiroNumero + segundoNumero;
+let subt :number = primeiroNumero - segundoNumero;
+let mult :number = primeiroNumero * segundoNumero;
+let divi :number = primeiroNumero / segundoNumero;
+let modo :number = primeiroNumero % segundoNumero;
+
+console.log(soma);  // 10
+console.log(subt);  // 6
+console.log(mult);  // 40
+console.log(divi);  // 2.5
+console.log(modo);  // 2
